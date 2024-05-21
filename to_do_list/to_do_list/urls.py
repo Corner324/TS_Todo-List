a6deab1c98030fcfff2fe9321a61d12ce440e8f1
Corner324 <mosenn@mail.ru>
2024-05-21 22:19:57 +0300

    Add swagger

--- to_do_list/to_do_list/urls.py
+++ to_do_list/to_do_list/urls.py
@@ -21,10 +21,32 @@ from django.urls import include, path
 from rest_framework import routers
 from todo.views import TodoViewSet
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
 router = routers.SimpleRouter()
 router.register(r"todo", TodoViewSet)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version="v1",
+        description="API description",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include(router.urls)),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
