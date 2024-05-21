9a229392ac9a4b5ea53b5a0e6f10cc56bcfc4c93
Corner324 <mosenn@mail.ru>
2024-05-21 22:06:56 +0300

    Add serializer & ViewSet

--- to_do_list/to_do_list/urls.py
+++ to_do_list/to_do_list/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework import routers
+from todo.views import TodoViewSet
+
+router = routers.SimpleRouter()
+router.register(r"todo", TodoViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include(router.urls)),
 ]
--- to_do_list/todo/views.py
+++ to_do_list/todo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import TodoSerializer
+
+from .models import Task
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TodoSerializer
