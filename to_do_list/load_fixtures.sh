68923bc1e4cfea5a813cfe6f68d7ecd3cdf222f4
Corner324 <mosenn@mail.ru>
2024-05-22 17:09:42 +0300

    Fix load fixtures 2

--- to_do_list/load_fixtures.sh
+++ to_do_list/load_fixtures.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python manage.py loaddata todo/fixtures/todo/tasks_fixture.json || echo "Фикстуры уже загружены или произошла ошибка."
-python manage.py loaddata todo/fixtures/todo/tags_fixture.json || echo "Фикстуры уже загружены или произошла ошибка."
+python manage.py loaddata todo/fixtures/todo/tasks_fixture.json || echo "Фикстуры уже загружены."
+python manage.py loaddata todo/fixtures/todo/tags_fixture.json || echo "Фикстуры уже загружены."
